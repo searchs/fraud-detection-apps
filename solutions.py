dfe1ddf3b10bd5999b713e11ff46788b949dd30f
Ola Ajibode <ola.ajibode@gmail.com>
2016-04-25 13:29:06 +0100

    Create solution draft

---
+++ solutions.py
@@ -0,0 +1,23 @@
+### Exercise 1
+``` python
+restuarant_list = ["Jamie's Italian", "Cabana", "Greek Island", "Obalende Suya"]
+drinks_list = ['Koko dry', 'Fanta', 'Sprite', 'Water']
+my_eating_box = restuarant_list + drinks_list
+my_eating_box.append("Caramel Ice cream")
+```
+
+### Exercise 2
+```
+bus_count = 20
+
+```
+
+
+
+### Exercise 3
+```
+firstName = input("Enter you first name please")
+lastName = input("Enter your last name please")
+print("Hello there, %s %s" %(firstName, lastName))
+
+```
