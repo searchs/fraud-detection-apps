b44ae9979d22595906631b307e0efeee6e1ce148
Ola Ajibode <ola.ajibode@gmail.com>
2016-07-25 17:32:48 +0100

    dictionary comprehension map and filter examples added

--- listbox.py
+++ listbox.py
@@ -40,8 +40,7 @@ for key,value in char_count.items():
 
 print(char_freq)
 
-# Comprehension 
-#
+#  List Comprehension 
 squared = []
 
 for n in range(11):
@@ -57,3 +56,44 @@ print(sqd)
 # More computation
 sqd_1 = [n**2 for n in range(11) if n % 2 is 0]
 print(sqd_1)
+
+# Dictionary Comprehension
+# Template
+# var_name = {
+#     key: value
+#     for loop_var in collection
+# }
+
+char_freqc =  {
+    key.lower(): char_count.get(key.lower(),0) +  char_count.get(key.upper(),0)
+
+for key in char_count.keys()
+}
+
+print(char_freqc)
+
+# Maps in Python
+# Template
+
+def sq2(r):
+    return (r*2)**2
+
+print(list(map(sq2, range(8))))
+
+
+# Filters in Python
+
+def grabs(e):
+    return  (e + abs(e))**3
+
+print(list(map(grabs, range(-5,6))))
+
+def strip_neg(q):
+    return q > 0
+
+print("Print filtered list")
+print(list(filter(strip_neg, range(-5,6))))
+
+print("Filter with in-built function")
+print(list(map(grabs, filter(strip_neg,range(-5,6)))))
+
