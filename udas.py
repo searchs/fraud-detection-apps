cdc5497c7991cb8e31072dded9bcc82f18cbf77d
Ola Ajibode <ola.ajibode@gmail.com>
2016-09-30 11:20:15 +0100

    Fixed same month bug

--- udas.py
+++ udas.py
@@ -4,13 +4,14 @@
 # Note that this will NOT produce correct ouptuts yet, since
 # our nextDay procedure assumes all months have 30 days
 # (hence a year is 360 days, instead of 365).
-# 
+#
+
 
 def nextDay(year, month, day):
     """Simple version: assume every month has 30 days"""
     if day < 30:
         day += 1
-        return year, month, day 
+        return year, month, day
     else:
         if month == 12:
             year += 1
@@ -18,34 +19,52 @@ def nextDay(year, month, day):
         else:
             month += 1
             return year, month, 1
-        
+
+
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1
        and year2/month2/day2. Assumes inputs are valid dates
        in Gregorian calendar, and the first date is not after
        the second."""
-        
     # YOUR CODE HERE!
     daysDiff = 0
-    if year1 == year2:
+    if  year1 == year2:
         if month1 == month2:
-            daysDiff = day2 -day1
+            # print "Equal Months"
+            daysDiff =  day2 - day1
+            return daysDiff
         else:
-            if year2 > year1:
-                daysDiff = (day2 - day1)                    
-    return daysDiff
+            # print "Different Months"
+            diff_days = (30 - day1) + day2
+            # print diff_days
+            # print "Month 2 is really greater"
+            diff_months = month2 - month1
+            # print diff_months
+            daysDiff = diff_days + 30*(diff_months -1)
+            # print  daysDiff
+            return daysDiff
+    elif year1 < year2:
+        # print "Year Two is greater"
+        years_diff = year2 - year1
+        # print years_diff
+        months_diff =  (12 - month1) + month2
+        # print months_diff
+        diff_days = (30 - day1) + day2
+        # print diff_days
+        daysDiff = diff_days + 30*(months_diff -1)
+        return daysDiff
 
 def test():
-    test_cases = [((2012,9,30,2012,10,30),30), 
+    test_cases = [((2012,9,30,2012,10,30),30),
                   ((2012,1,1,2013,1,1),360),
                   ((2012,9,1,2012,9,4),3)]
-    
+
     for (args, answer) in test_cases:
         result = daysBetweenDates(*args)
         if result != answer:
             print "Test with data:", args, "failed"
         else:
-            print "Test case passed!"
+            print args,"Test case passed!"
 
 test()
-    
+
