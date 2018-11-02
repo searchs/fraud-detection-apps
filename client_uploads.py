303088b579455044849a0aee238fafc1494411d7
Ola Ajibode <ola.ajibode@gmail.com>
2018-11-02 13:56:12 +0000

    Clean uploads dir upon page load.

--- client_uploads.py
+++ client_uploads.py
@@ -5,24 +5,32 @@ from werkzeug.utils import secure_filename
 __author__ = 'Ola Ajibode'
 
 UPLOAD_FOLDER = 'uploads'
+global upload_queue
+upload_queue = []
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+target = os.path.join(APP_ROOT, UPLOAD_FOLDER)
+
 ALLOWED_EXTENSIONS = {'csv', 'tsv', 'sql'}
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(os.listdir(target))
+    clean_uploads(target)
+
     return render_template('index.html')
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    target = os.path.join(APP_ROOT, UPLOAD_FOLDER)
+    # target = os.path.join(APP_ROOT, UPLOAD_FOLDER)
     print(target)
 
     if not os.path.isdir(target):
         os.mkdir(target)
+    clean_uploads(target)
 
     for upp_file in request.files.getlist('file'):
         print(upp_file)
@@ -31,11 +39,22 @@ def upload():
         print(destination)
         upp_file.save(destination)
 
+    upqueue = os.listdir(target)
+    print(upqueue)
+
     return render_template('complete.html')
 
 
-def clean_uploads():
-    pass
+def clean_uploads(target):
+    upload_queue = os.listdir(target)
+    print(upload_queue)
+    if len(upload_queue) > 0:
+        for f in upload_queue:
+            os.remove(os.path.join(target,f))
+    upload_queue = os.listdir(target)
+    print(upload_queue)
+
+
 
 
 def push_files_s3():
