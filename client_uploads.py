cf1d5c45d356a494b7123ad8e91117e124573c34
Ola Ajibode <ola.ajibode@gmail.com>
2018-11-21 17:43:23 +0000

    Error page handling added

--- client_uploads.py
+++ client_uploads.py
@@ -1,70 +1,126 @@
+from __future__ import print_function
 import os
 
-from flask import Flask, render_template, flash, request, redirect, url_for
-# from werkzeug.utils import secure_filename
-from flask_s3 import FlaskS3
 
-__author__ = 'Ola Ajibode'
+from flask import render_template, request, flash
+from programmatic_self_uploader import app
 
-ALLOWED_EXTENSIONS = {'csv', 'tsv', 'sql'}
+ALLOWED_EXTENSIONS = {'csv', 'tsv'}
 UPLOAD_FOLDER = 'uploads'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+TARGET = os.path.join(APP_ROOT, UPLOAD_FOLDER)
 
-target = os.path.join(APP_ROOT, UPLOAD_FOLDER)
+if not os.path.isdir(TARGET):
+    os.mkdir(TARGET)
 
-upload_queue = os.listdir(target)
-
-app = Flask(__name__)
-app.config['FLASKS3_BUCKET_NAME'] = 'reports-adhoc-raw'
-s3 = FlaskS3(app)
+UPLOAD_QUEUE = os.listdir(TARGET)
+RAW_BUCKET = 'adhoc-raw'
+S3_BUCKET = "lytics-programmatic-automation-report"
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/", methods=["GET"])
 def index():
-    print(dir(s3.init_app))
-    if len(upload_queue) > 0:
-        clean_uploads(target)
-    return render_template('index.html', title='Ingest File Upload')
+    if UPLOAD_QUEUE:
+        clean_uploads()
+    return render_template('index.html',
+                           env=app.config['ohprice_environment'],
+                           title='Raw File Upload')
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    if not os.path.isdir(target):
-        os.mkdir(target)
+    if not os.path.isdir(TARGET):
+        os.mkdir(TARGET)
 
-    if len(request.files.getlist('file')) == 0:
-        # TODO: Remove print, move to logger
-        print("No file selected for upload.  Reloading page.....")
-        return render_template('index.html', title='No File Selected')
+    try:
+        clean_uploads()
+    except OSError:
+        flash('File not found in directory or no directory: {}'.format(TARGET))
 
-    for upp_file in request.files.getlist('file'):
-        filename = upp_file.filename
-        destination = "/".join([target, filename])
-        # Remove print, move to logger
-        print(destination)
-        upp_file.save(destination)
+    target_date = request.form['report-date']
+    report_type = request.form['selectProcess']
 
-    upqueue = os.listdir(target)
-    return render_template('complete.html', upload_list=upqueue, title='Upload Status')
+    if report_type == 'Rerun A Report':
+        flash("No new data hence no upload necessary")
+        upload_details, message = rerun_report()
+    elif report_type == 'Run New Report':
+        upload_details, message = run_new_report()
+
+    flash(upload_details)
+
+    if not any(upload_details):
+        flash("Empty params!")
+        message = "No file uploaded or no date supplied.  Try again."
+        return render_template('index.html',
+                               env=app.config['ohprice_environment'],
+                               message=message)
+
+    return render_template('complete.html',
+                           env=app.config['ohprice_environment'],
+                           title='Upload Status',
+                           target_date=target_date,
+                           upload_details=upload_details,
+                           message=message)
 
 
-def clean_uploads(target):
-    upload_queue = os.listdir(target)
-    # Remove print, move to logger
-    if len(upload_queue) > 0:
-        for f in upload_queue:
-            os.remove(os.path.join(target, f))
-    upload_queue = os.listdir(target)
+def run_new_report():
+    upload_details = dict()
+    try:
+        for upp_file in request.files.getlist('file'):
+            filename = upp_file.filename
+            destination = "/".join([TARGET, filename])
+            upp_file.save(destination)
+            details = dict()
+            details['filesize'] = round(float(os.stat(destination).st_size / 1024 / 1024), 2)
+            details['target_date'] = request.form['report-date']
+            details['lookback'] = request.form['lookback']
+            upload_details[filename] = details
+            print(upload_details)
 
-    if len(upload_queue) > 0:
+        if upload_details:
+            message = "Upload Successful! "
+        else:
+            message = "No file selected.  Try again please."
+
+    except OSError:
+        message = "No file selected. No file uploaded."
+        print("File does not exists in uploads directory!")
+    return upload_details, message
+
+
+def rerun_report():
+    report_details = {}
+    details = dict()
+    details['target_date'] = request.form['report-date']
+    details['lookback'] = request.form['lookback']
+    report_details['rerun'] = details
+    print(report_details)
+    message = "Report Rerun is now in progress..."
+
+    return report_details, message
+
+
+@app.route('/check', methods=['GET'])
+def check():
+    return render_template('check.html',
+                           title='App Status',
+                           env=app.config['ohprice_environment'])
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # note that we set the 404 status explicitly
+    return render_template('404.html',
+                           message=error,
+                           env=app.config['ohprice_environment']), 404
+
+
+def clean_uploads():
+    upload_queue = os.listdir(TARGET)
+    if upload_queue:
+        for item in upload_queue:
+            os.remove(os.path.join(TARGET, item))
+        upload_queue = os.listdir(TARGET)
+
+    if upload_queue:
         print("Remove file failed. Upload dir still contains files.")
-
-
-def push_files_s3():
-    pass
-
-
-if __name__ == '__main__':
-    # Remove files in upload dir
-    app.run(host='127.0.0.1', port=8000, debug=True)
