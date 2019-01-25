604fcfc853fdb5ce3c5396e365bd64f8dd1b6719
Ola Ajibode <ola.ajibode@gmail.com>
2019-01-25 23:42:56 +0000

    Creat audiobox.py

---
+++ audiobox.py
@@ -0,0 +1,56 @@
+import pyglet
+import pyaudio
+import wave
+import speech_recognition as sr
+
+
+def play_audio(filename):
+    chunk = 1024
+    wf = wave.open(filename, 'rb')
+    pa = pyaudio.PyAudio()
+
+    stream = pa.open(
+        format=pa.get_format_from_width(wf.getsampwidth()),
+        channels=wf.getnchannels(),
+        rate=wf.getframerate(),
+        output=True)
+
+    data_stream = wf.readframes(chunk)
+    while data_stream:
+        stream.write(data_stream)
+        data_stream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate()
+
+
+# '''
+# file = pyglet.resource.media('audio/ohyeah.wav')
+# file.play()
+# pyglet.app.run()
+# '''
+
+r = sr.Recognizer()
+
+
+def initSpeech():
+    print("Listening....")
+    play_audio("./audio/okay.wav")
+    with sr.Microphone() as source:
+        print("Say Something ...")
+        audio = r.listen(source)
+
+    play_audio("./audio/yes.wav")
+
+    command = ""
+
+    try:
+        command = r.recognize_google(audio)
+    except:
+        print("Could not understand you, bro")
+
+    print("Your command")
+    print(command)
+
+
+initSpeech()
