410e4792b58001bba89336a71241233e53897743
Ola Ajibode <ola.ajibode@gmail.com>
2023-07-27 13:23:48 +0100

    Create console_print.py

---
+++ console_print.py
@@ -0,0 +1,20 @@
+"""Print details e.g. customer record, to the console in formatted view""" 
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = (
+        f"| Name: {passenger} |"
+        f" Flight: {flight_number} |"
+        f" Seat: {seat} |"
+        f" Aircraft: {aircraft}"
+        " |"
+    )
+    banner = "+" + "-" * (len(output) - 2) + "+"
+    border = "|" + " " * (len(output) - 2) + "|"
+
+    lines = [banner, border, output, border, banner]
+    card = "\n".join(lines)
+    print(card)
+    print()
+
+
+# Check output
+console_card_printer("Ola Traveller", "A6", "NG1029", "Airbus A319")
